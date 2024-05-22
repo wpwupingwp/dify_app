a587d72b7f382a4075c56d529f831c32b1a3eec9
DL <wpwupingwp@outlook.com>
2024-05-22 15:57:02 +0800

    use cuda for clip-iqa

---
+++ src/score.py
@@ -0,0 +1,48 @@
+from loguru import logger as log
+from pathlib import Path
+from torchmetrics.multimodal import CLIPImageQualityAssessment as clip_iqa
+
+import loguru
+import torch
+import torchvision
+
+def scan_files(workdir=None) -> Path:
+    if workdir is None:
+        workdir = '/media/ping/Data/Work/pics'
+    for folder in Path(workdir).glob('*'):
+        for img in folder.glob('*'):
+            if img.suffix in {'.jpg', '.png', '.jpeg'}:
+                yield img
+
+
+log.add('file_{time}.log')
+log.info('Start')
+prompts=('quality', 'noisiness', 'sharpness','natural','complexity', 
+         ('plant photo','not plant photo'), 
+         ('flower photo', 'not flower photo'),
+         ('plant leaf photo', 'not plant leaf photo'))
+# m = clip_iqa(prompts=prompts, model_name_or_path='openai/clip-vit-base-patch32')
+m = clip_iqa(prompts=prompts,
+             model_name_or_path='openai/clip-vit-large-patch14').to('cuda')
+log.info('Model loaded')
+log.info(f'Prompts:{prompts}')
+
+result = list()
+for img_file in scan_files():
+    # unsqueeze to 4-d tensor 
+    # img = torch.randint(255,(2,3,224,224)).float()
+    img = torchvision.io.read_image(img_file).unsqueeze(0)
+    s = m(img)
+    #s = m(torch.tensor(img))
+    values = [f'{i.item():.2f}' for i in s.values()]
+    log.info(f'{img_file.stem}, {values}')
+    result.append((img_file, *values))
+with open('result.txt', 'w') as out:
+    out.write(','.join(prompts))
+    for i in result:
+        name = i[0]
+        values = i[1:]
+        v = ','.join([f'{j:.2f}' for j in values])
+        out.write(f'{name},{v}\n')
+log.info('Done')
+
