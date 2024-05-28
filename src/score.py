26577f81ffbed64f207ce263d0633e3a7220b025
DL <wpwupingwp@outlook.com>
2024-05-28 17:13:31 +0800

    rewrite to more clean

--- src/score.py
+++ src/score.py
@@ -1,42 +1,82 @@
 from loguru import logger as log
 from pathlib import Path
 from torchmetrics.multimodal import CLIPImageQualityAssessment as clip_iqa
+from shutil import copy, rmtree
 
-import loguru
-import torch
 import torchvision
 
+model = 'openai/clip-vit-base-patch32'
+# model = 'openai/clip-vit-large-patch14'
+prompts = [('Good photo.', 'Bad photo.'),
+           ('Complex photo.', 'Simple photo.'),
+           ('Beautiful photo.', 'Ugly photo.')
+           ('Aesthetic photo.', 'Not aesthetic photo.'),
+           ('Plant photo.', 'not plant photo.') ]
+output_file = open('result.txt', 'w')
+output_folder = Path('/tmp/out')
+
 def scan_files(workdir=None) -> Path:
+    count = 0
+    PAUSE = 100
     if workdir is None:
         workdir = '/media/ping/Data/Work/pics'
     for folder in Path(workdir).glob('*'):
         for img in folder.glob('*'):
             if img.suffix in {'.jpg', '.png', '.jpeg'}:
                 yield img
+                count += 1
+                if count > PAUSE:
+                    pass
 
 
-log.add('file_{time}.log')
-log.info('Start')
-prompts=('quality', 'noisiness', 'sharpness','natural','complexity', 
-         ('plant photo','not plant photo'), 
-         ('flower photo', 'not flower photo'),
-         ('plant leaf photo', 'not plant leaf photo'))
-# m = clip_iqa(prompts=prompts, model_name_or_path='openai/clip-vit-base-patch32')
-m = clip_iqa(prompts=prompts,
-             model_name_or_path='openai/clip-vit-large-patch14').to('cuda')
-log.info('Model loaded')
-log.info(f'Prompts:{prompts}')
+def analyze(img_score: list, output_folder: Path):
+    img_score.sort(key=lambda x:float(x[1][0]))
+    last_score = 'x'
+    count = 0
+    for r in img_score:
+        img, score, *_ = r
+        s1 = str(score)[2]
+        if s1 != last_score:
+            count = 0
+            last_score = s1
+        if count > 9:
+            continue
+        p = Path(img)
+        copy(img, output_folder/(s1+'-'+p.name))
+        count += 1
+        log.info(r)
 
-out = open('result.txt', 'w')
-out.write(str(prompts)+'\n')
-for img_file in scan_files():
-    # unsqueeze to 4-d tensor 
-    # img = torch.randint(255,(2,3,224,224)).float()
-    img = torchvision.io.read_image(img_file).unsqueeze(0).to('cuda')
-    s = m(img)
-    #s = m(torch.tensor(img))
-    values = ','.join([f'{i.item():.2f}' for i in s.values()])
-    log.info(f'{img_file.stem}, {values}')
-    out.write(f'{img_file},{values}\n')
-log.info('Done')
 
+def main():
+    log.add('iqa_{time}.log')
+    log.info('Start')
+    m = clip_iqa(prompts=prompts, model_name_or_path=model).to('cuda')
+    log.info('Model loaded')
+    log.info(f'Prompts:{prompts}')
+
+    output_file.write(str(prompts)+'\n')
+    if not output_folder.exists():
+        output_folder.mkdir()
+    else:
+        log.warning('Clean old output')
+        rmtree(output_folder)
+    log.info(f'Output folder {output_folder}')
+
+    log.info('Analyzing')
+    img_score = list()
+    for n, img_file in enumerate(scan_files()):
+        log.info(f'{n} {img_file}')
+        # unsqueeze to 4-d tensor 
+        img = torchvision.io.read_image(img_file).unsqueeze(0).to('cuda')
+        s = m(img)
+        values = s.values()
+        values_str = ','.join([f'{i.item():.2f}' for i in values])
+        output_file.write(f'{img_file},{values_str}\n')
+
+        img_score.append((img_file, values))
+    log.info('Sorting score')
+    analyze(img_score, output_folder)
+    log.info('Done')
+
+
+main()
